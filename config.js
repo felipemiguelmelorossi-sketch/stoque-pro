b419ed041ba99a8fbe7ad6489b306ef1c3e67cb6
felipemiguelmelorossi-sketch <felipemiguelmelorossi@gmail.com>
2026-09-15 14:39:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_SUPABASE";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_DO_SEU_SUPABASE";
+const SUPABASE_ANON_KEY = "sb_publishable_zZy8CirGsWqjzYIv2GIanA_BQaASny8";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
