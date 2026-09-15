db7d60d1fa0051f09d77e9024ebbbbf86a08a1a9
felipemiguelmelorossi-sketch <felipemiguelmelorossi@gmail.com>
2026-09-15 15:17:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://dnojapjtsgxsgmxxjhrp.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://dnojapjtsgxsgmxxjhrp.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_zZy8CirGsWqjzYIv2GIanA_BQaASny8";
 
 const supabaseClient = window.supabase.createClient(
